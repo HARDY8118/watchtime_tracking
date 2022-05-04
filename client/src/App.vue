34eda017df9dfbc77bbab1ffcf5074927381d28b
Harshit Vishnoi <5825564-HARDY8118@users.noreply.gitlab.com>
2022-05-05 00:43:24 +0530

    Added carousel

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,16 @@
 <script>
 
 import * as Fingerprint from "@fingerprintjs/fingerprintjs";
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
 
 export default {
   data() {
     return {
       status: "loading",
-      videos: []
+      videos: [],
+      currentSlide: 0,
     }
   },
   methods: {
@@ -28,11 +32,15 @@ export default {
     } catch (e) {
       console.error(e)
     }
-
-    // document.querySelector('.carousel-item').classList.add('active')
   },
   async created() {
     this.videos = (await (await fetch("http://localhost:8080/list/videos")).json())
+  },
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
   }
 }
 </script>
@@ -45,8 +53,8 @@ export default {
         <br>
       </b-alert>
     </template>
-    <div>
-      <b-carousel id="carousel-fade" style="text-shadow: 0px 0px 2px #000; height: 100vh;" fade controls indicators
+    <div style="display: flex; justify-content: center;">
+      <!-- <b-carousel id="carousel-fade" style="text-shadow: 0px 0px 2px #000; height: 100vh;" fade controls indicators
         img-width="1024" img-height="840">
         <b-carousel-slide :caption="v.title" v-for="(v, i) in videos" :key="i" style="height: 100vh;">
           <video>
@@ -55,13 +63,30 @@ export default {
           <h2>{{ v.title }}</h2>
           <h3>By: {{ v.videographer }}</h3>
         </b-carousel-slide>
-      </b-carousel>
+      </b-carousel> -->
+
+      <carousel style="width: 90vw; height: 100vh;" :itemsToShow="1" :wrapAround="true">
+        <slide v-for="({ videographer, title, file }, i) in videos" :key="i"
+          style="display: flex; flex-direction: column;">
+          <h2>{{ title }}</h2>
+          <br>
+          <video autoplay muted style="height: 40vw;">
+            <source :src="'http://127.0.0.1:8080/videos/' + file">
+          </video>
+          <br>
+          <h3>By: {{ videographer }}</h3>
+        </slide>
+
+        <template #addons="{ slidesCount, currentSlide }">
+          <navigation v-if="slidesCount > 1" />
+          <!-- <pagination /> -->
+          <!-- {{ currentSlide }} -->
+          <span ref="current" :current="currentSlide"></span>
+        </template>
+      </carousel>
     </div>
   </b-overlay>
-
-  <!-- <video controls>
-    <source src="http://localhost:8080/videos/pexels-вальдемар-10026607.mp4">
-  </video> -->
+  <h1>Slide: {{ currentSlide }} </h1>
 
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import BootstrapVue3 from "bootstrap-vue-3"
+import { createApp } from "vue";
+import App from "./App.vue";
+import BootstrapVue3 from "bootstrap-vue-3";
+import "fullpage.js/vendors/scrolloverflow";
+import VueFullPage from "vue-fullpage.js";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-createApp(App).use(BootstrapVue3).mount('#app')
+createApp(App).use(BootstrapVue3).use(VueFullPage).mount("#app");
